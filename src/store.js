6bef9c549475be3d0f92108029e6c4212ba6f494
Alexander Castillo <p.alexcast@gmail.com>
2019-03-29 17:06:51 +0100

    Remove countries.json
    Fetch countries directly from svg paths.
    Cleaned up code for updating isVisited as well.

--- src/store.js
+++ src/store.js
@@ -1,14 +1,14 @@
-import Vue from 'vue';
-import VueX from 'vuex';
-import createPersistedState from 'vuex-persistedstate';
-import Countries from './assets/countries.json';
+import Vue from "vue";
+import VueX from "vuex";
+import createPersistedState from "vuex-persistedstate";
+import MapImg from "./assets/world.svg";
 
 Vue.use(VueX);
 
 const store = new VueX.Store({
   plugins: [createPersistedState()],
   state: {
-    searchText: '',
+    searchText: "",
     countries: []
   },
   mutations: {
@@ -20,29 +20,25 @@ const store = new VueX.Store({
     },
     TOGGLE_VISITED(
       state,
-      { countryIndex, countryName } = { countryIndex: -1, countryName: '' }
+      { countryIndex, countryName } = { countryIndex: -1, countryName: "" }
     ) {
       const indexWasPassed = countryIndex > -1;
       const nameWasPassed = countryName && countryName.length > 0;
 
-      state.countries = state.countries.map((country, index) => {
-        let correctCountry = false;
-
-        if (indexWasPassed) {
-          correctCountry = index === countryIndex;
-        } else if (nameWasPassed) {
-          correctCountry = country.name === countryName;
-        }
-
-        if (!correctCountry) {
-          return country;
-        }
-
-        return {
-          ...country,
-          isVisited: !country.isVisited
-        };
-      });
+      let index;
+      if (indexWasPassed && state.countries.length > indexWasPassed) {
+        index = countryIndex;
+      } else if (nameWasPassed) {
+        index = state.countries.findIndex(
+          country => country.name === countryName
+        );
+      }
+      const country = state.countries[index];
+      if (!country) {
+        return;
+      }
+      country.isVisited = !country.isVisited;
+      state.countries[index] = country;
     }
   },
   actions: {
@@ -53,19 +49,24 @@ const store = new VueX.Store({
         return state.countries;
       }
 
-      const countries = Countries.map((country, index) => ({
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(MapImg, "image/svg+xml");
+      const paths = [...doc.getElementsByTagName("path")];
+      const countryNames = paths.map(path => path.getAttribute("data-name"));
+      paths.sort();
+      const countries = countryNames.map((country, index) => ({
         index,
         name: country,
         isVisited: false
       }));
 
-      commit('SET_COUNTRIES', countries);
+      commit("SET_COUNTRIES", countries);
     },
     setSearchText({ commit }, searchText) {
-      commit('SET_SEARCH_TEXT', searchText);
+      commit("SET_SEARCH_TEXT", searchText);
     },
     toggleVisited({ commit }, countryIndex) {
-      commit('TOGGLE_VISITED', countryIndex);
+      commit("TOGGLE_VISITED", countryIndex);
     }
   },
   getters: {
