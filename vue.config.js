3cf86469affb98e8477696cf2bcc0033cf02561f
Sindre Bøyum <sindre.boyum@tietoevry.com>
2023-06-20 12:13:04 +0200

    feat: update vue

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,20 @@
-const path = require('path')
-const PrerenderSPAPlugin = require('prerender-spa-plugin')
+import { join } from 'path';
+import PrerenderSPAPlugin from 'prerender-spa-plugin';
 
-module.exports = {
-  chainWebpack: (config) => {
-    const svgRule = config.module.rule('svg');
+export function chainWebpack(config) {
+  const svgRule = config.module.rule('svg');
 
-    svgRule.uses.clear();
+  svgRule.uses.clear();
 
-    svgRule
-      .use('raw-loader')
-      .loader('raw-loader');
-  },
-  configureWebpack: {
-    plugins: [
-      new PrerenderSPAPlugin({
-        staticDir: path.join(__dirname, 'dist'),
-        routes: ['/'],
-      }),
-    ]
-  }
+  svgRule
+    .use('raw-loader')
+    .loader('raw-loader');
+}
+export const configureWebpack = {
+  plugins: [
+    new PrerenderSPAPlugin({
+      staticDir: join(import.meta.url, 'dist'),
+      routes: ['/'],
+    }),
+  ]
 };
